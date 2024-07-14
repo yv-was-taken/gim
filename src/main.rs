0ae5ed0be8fc63f259c19cf5fc1a7653088b881e
ywvlfy <yvmail@proton.me>
2024-07-14 17:40:28 -0500

    refactor input to take quotes instead of plain text

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 use std::io;
+use std::str::FromStr;
 
 fn main() -> io::Result<()> {
     let mut input = String::new();
@@ -6,35 +7,53 @@ fn main() -> io::Result<()> {
 
     let mut input_iter = input.split_whitespace();
     let command = input_iter.next();
-    let arg = input_iter.next();
+    let rest_of_input = input_iter.collect::<Vec<&str>>().join(" ");
 
-    if !input_iter.next().is_none() {
+    let arg = extract_quoted_string(&rest_of_input);
+
+    if command.is_none() || (arg.is_none() && rest_of_input != "") {
         return Err(io::Error::new(
             io::ErrorKind::InvalidInput,
-            "Too many inputs!",
+            "Invalid input format!",
         ));
-    } else {
-        match command {
-            Some(value) => match value {
-                "plan" => plan(&arg.unwrap()),
-                "push" => push(),
-                _ => Err(io::Error::new(
-                    io::ErrorKind::InvalidInput,
-                    "Unrecognized command",
-                )),
-            },
+    }
 
-            None => {
-                println!("standard git status type thing here");
-                Ok(())
+    match command {
+        Some("plan") => {
+            if let Some(argument) = arg {
+                plan(&argument)
+            } else {
+                Err(io::Error::new(
+                    io::ErrorKind::InvalidInput,
+                    "Argument is missing or invalid!",
+                ))
             }
         }
+        Some("push") => push(),
+        Some(_) => Err(io::Error::new(
+            io::ErrorKind::InvalidInput,
+            "Unrecognized command",
+        )),
+        None => {
+            println!("standard git status type thing here");
+            Ok(())
+        }
+    }
+}
+
+fn extract_quoted_string(input: &str) -> Option<String> {
+    if input.starts_with('"') && input.ends_with('"') {
+        Some(String::from_str(&input[1..input.len() - 1]).unwrap())
+    } else if input.starts_with('\'') && input.ends_with('\'') {
+        Some(String::from_str(&input[1..input.len() - 1]).unwrap())
+    } else {
+        None
     }
 }
 
 pub fn plan(arg: &str) -> io::Result<()> {
     // @TODO write input arg as commit message, to repo-specific filename
-    println!("your plan is: {arg}");
+    println!("your plan is: {}", arg);
     Ok(())
 }
 
