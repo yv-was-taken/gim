16f36e8dc51fb80b53a154788d4e8ba4c5fa75a5
ywvlfy <yvmail@proton.me>
2024-07-14 07:00:57 -0500

    impl flow skeleton

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,49 @@
-fn main() {
-    println!("Hello, world!");
+use std::io;
+// use std::io::{/* ErrorKind */ };
+//use std::io::prelude::*;
+//use std::fs::File;
+//use std::error::Error;
+
+//use std::io::{self, Read};
+
+fn main() -> io::Result<()> {
+    let mut input = String::new();
+    io::stdin().read_line(&mut input)?;
+
+    let mut input_iter = input.split_whitespace();
+    let command = input_iter.next();
+    let arg = input_iter.next();
+
+    if !input_iter.next().is_none() {
+        return Err(io::Error::new(
+            io::ErrorKind::InvalidInput,
+            "Too many inputs!",
+        ));
+    } else {
+        match command {
+            Some(value) => match value {
+                "plan" => plan(&arg.unwrap()),
+                "push" => push(),
+                _ => Err(io::Error::new(
+                    io::ErrorKind::InvalidInput,
+                    "Unrecognized command",
+                )),
+            },
+
+            None => {
+                println!("standard git status type thing here");
+                Ok(())
+            }
+        }
+    }
 }
 
+pub fn plan(arg: &str) -> io::Result<()> {
+    println!("your plan is: {arg}");
+    Ok(())
+}
 
-fn 
-
-
-
+pub fn push() -> io::Result<()> {
+    // read commit file, push it
+    Ok(())
+}
